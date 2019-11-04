3ac5d11d753f76daed3fbb960f8d023e5f227f49
Huda Yousif <hjdoski@gmail.com>
2019-11-04 19:56:58 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(total_tardies_for_all_students) FRON students;"
+  "SELECT COUNT(total_tardies_for_all_students) FROM students;"
 end
 
 def average_gpa_for_9th_grade
